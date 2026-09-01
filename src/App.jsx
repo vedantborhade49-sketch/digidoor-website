c5499924640a9d3747438cd5b43c5fb9978a49dc
unknown <guptabhumit9744@gmail.com>
2026-09-01 12:21:18 +0530

    Revert footer to normal standard footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Footer from './components/footer/CinematicFooter';
+import Footer from './components/Footer';
 import ScrollToTop from './components/ScrollToTop';
 
 import Home from './pages/Home';
